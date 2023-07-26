eb91127a912767c7122335ff31fb13fc049ff665
ItsgalinaL <S.galina.l@yandex.ru>
2023-07-26 18:33:03 +0700

    Ещё исправление замечаний

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,11 +1,37 @@
 package ru.netology.data;
 
+import lombok.Value;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class DataHelper {
 
-    private DataHelper() {
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
     }
 
-    public static User getUser() {
-        return new User("vasya", "qwerty123", "12345", new String[]{"5559 0000 0000 0001", "5559 0000 0000 0002"});
+    public AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    public VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class UserInfo {
+        String[] cards;
+    }
+
+    public UserInfo getUserInfo(AuthInfo authInfo) {
+        return new UserInfo(new String[]{"5559 0000 0000 0001", "5559 0000 0000 0002"});
     }
 }
--- src/test/java/ru/netology/test/CardsTest.java
+++ src/test/java/ru/netology/test/CardsTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
-import ru.netology.data.User;
 import ru.netology.pages.CardsPage;
 import ru.netology.pages.LoginPage;
 
@@ -15,17 +14,18 @@ public class CardsTest {
     @Test
     @DisplayName("Тест перевода")
     void testCards() {
-        User user = DataHelper.getUser();
-
+        DataHelper.AuthInfo authInfo = DataHelper.getAuthInfo();
+        DataHelper.VerificationCode verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         LoginPage loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
-        CardsPage cardsPage = loginPage.login(user.getLogin(), user.getPassword()).verifyCode(user.getValidationCode());
+        CardsPage cardsPage = loginPage.login(authInfo.getLogin(), authInfo.getPassword()).verifyCode(verificationCode.getCode());
 
-        int firstCardBalance = cardsPage.getCardBalance(user.getCards()[0]);
-        int secondCardBalance = cardsPage.getCardBalance(user.getCards()[1]);
+        DataHelper.UserInfo userInfo = DataHelper.getUserInfo(authInfo);
+        int firstCardBalance = cardsPage.getCardBalance(userInfo.getCards()[0]);
+        int secondCardBalance = cardsPage.getCardBalance(userInfo.getCards()[1]);
 
-        cardsPage = cardsPage.topUp(user.getCards()[0]).transfer("200", user.getCards()[1]);
+        cardsPage = cardsPage.topUp(userInfo.getCards()[0]).transfer("200", userInfo.getCards()[1]);
 
-        assertEquals(firstCardBalance + 200, cardsPage.getCardBalance(user.getCards()[0]), "Баланс должен быть 10200");
-        assertEquals(secondCardBalance - 200, cardsPage.getCardBalance(user.getCards()[1]), "Баланс должен быть 9800");
+        assertEquals(firstCardBalance + 200, cardsPage.getCardBalance(userInfo.getCards()[0]), "Баланс должен быть 10200");
+        assertEquals(secondCardBalance - 200, cardsPage.getCardBalance(userInfo.getCards()[1]), "Баланс должен быть 9800");
     }
 }
